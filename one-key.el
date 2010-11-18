e20cf5ce66b103ad2ab64d0b51977c6b13dc5be7
vapniks <vapniks@yahoo.com>
2010-11-18 19:23:41 +0000

    Changed names of some variables from one-key-menu-???? to one-key-???
    git-svn-id: https://joebloggs.svn.cvsdude.com/odesk/one-key@125 f1a581bd-6a14-4726-baf4-9ff2dff07489

--- one-key.el
+++ one-key.el
@@ -79,7 +79,7 @@
 ;;   (interactive)
 ;;   (one-key-menu "EMMS" one-key-menu-emms-alist t))
 ;;
-;; Add an item to `one-key-menu-toplevel-alist' in the customization buffer for one-key
+;; Add an item to `one-key-toplevel-alist' in the customization buffer for one-key
 ;; (M-x customize-group RET one-key RET). The first item should be the key (e.g. m), the second item
 ;; should be a description (e.g. "Emacs multimedia system"), and the third item should be the command:
 ;; `one-key-menu-emms'. Then bind `one-key-menu-toplevel' to any key you want E.g:
@@ -94,7 +94,7 @@
 ;; Now when you type the key, a one-key menu will popup at the bottom of the window.
 ;; Then you just type a keystroke listed in the menu to execute the corresponding command.
 ;;
-;; You can also associate menus with major-modes using the customizable `one-key-menu-mode-alist' variable, 
+;; You can also associate menus with major-modes using the customizable `one-key-mode-alist' variable, 
 ;; and the `one-key-get-menu' command. When this command is run it will open the menu associated with the 
 ;; current major-mode, or the toplevel menu if there is no associated menu.
 ;; You can bind this to a global key, e.g:
@@ -190,7 +190,7 @@
 ;; If you used `one-key-show-template' the code is placed in the special buffer "One-Key-Template"
 ;; which has it's own one-key menu and keybindings bound to special helper functions to help you edit the
 ;; menu. Type M-x one-key-get-menu to see a menu of commands/keybindings for this buffer
-;; (or use one-key-menu-one-key-template if it is not listed in one-key-menu-mode-alist).
+;; (or use one-key-menu-one-key-template if it is not listed in one-key-mode-alist).
 ;; For example you can move items in the menu up/down using "M-<up>" or "M-<down>".
 ;; You can sort the items in the currently active region alphabetically by description/key binding/command
 ;; by pressing "C-c C-s" followed by d/k/c.
@@ -198,8 +198,8 @@
 ;;
 ;; Auto-load one-key menus:
 ;;
-;; If you set `one-key-menu-auto-load-menus' to t (in the customization group for one-key), then any files
-;; in the directory specified by `one-key-menu-menus-location' that match the regexp `one-key-menu-menus-regexp'
+;; If you set `one-key-auto-load-menus' to t (in the customization group for one-key), then any files
+;; in the directory specified by `one-key-menus-location' that match the regexp `one-key-menus-regexp'
 ;; will automatically be loaded on startup.
 
 ;;; Installation:
@@ -230,11 +230,11 @@
 ;; `one-key-help-window-max-height' : the maximal height use in popup window.
 ;; `one-key-items-per-line' : number of items in one line, if this option is `nil', will be calculated by `window-width'.
 ;; `one-key-keystroke-face' : face for highlighting keystroke
-;; `one-key-menu-auto-load-menus' : if t then automatically load one-key menus from `one-key-menu-menus-location'
-;; `one-key-menu-menus-location' : location in which one-key menus will be stored
-;; `one-key-menu-menus-regexp' : regexp to match filenames of one-key menus
-;; `one-key-menu-mode-alist' : An alist of major-mode, one-key menu pairs to set the default menu for each major-mode.
-;; `one-key-menu-toplevel-alist' : A list of key items for the toplevel menu.
+;; `one-key-auto-load-menus' : if t then automatically load one-key menus from `one-key-menus-location'
+;; `one-key-menus-location' : location in which one-key menus will be stored
+;; `one-key-menus-regexp' : regexp to match filenames of one-key menus
+;; `one-key-mode-alist' : An alist of major-mode, one-key menu pairs to set the default menu for each major-mode.
+;; `one-key-toplevel-alist' : A list of key items for the toplevel menu.
 ;; `one-key-popup-window' : whether to popup window when first time run, default is `t'.
 ;; `one-key-prompt-face' : face for highlighting prompt
 ;; `one-key-template-buffer-name' : the buffer name of the template code.
@@ -258,13 +258,13 @@
 ;;          `one-key-menu-one-key-template', `one-key-menu-one-key'
 ;;       * Added keybindings for `one-key-template-mode'.
 ;;       * Altered `one-key-help-format' function so that the keys are ordered by column instead of by row.
-;;       * Added `one-key-menu-toplevel-alist' customizable variable and `one-key-menu-toplevel' function.
-;;       * Added `one-key-menu-mode-alist' customizable variable and `one-key-get-menu' function.
+;;       * Added `one-key-toplevel-alist' customizable variable and `one-key-menu-toplevel' function.
+;;       * Added `one-key-mode-alist' customizable variable and `one-key-get-menu' function.
 ;;       * Alterend `one-key-insert-template' and `one-key-show-template' functions so that they also add
-;;         optional (commented) code to add items to `one-key-menu-mode-alist' and `one-key-menu-toplevel-alist'
-;;       * Added customization variables `one-key-menu-menus-location', `one-key-menu-menus-regexp' and
-;;         `one-key-menu-auto-load-menus', and function `one-key-menu-load-files'.
-;;         Added code to automatically load menus if `one-key-menu-auto-load-menus' is set to t.
+;;         optional (commented) code to add items to `one-key-mode-alist' and `one-key-toplevel-alist'
+;;       * Added customization variables `one-key-menus-location', `one-key-menus-regexp' and
+;;         `one-key-auto-load-menus', and function `one-key-load-files'.
+;;         Added code to automatically load menus if `one-key-auto-load-menus' is set to t.
 ;;       * Fixed spelling mistakes in documentation and added documentation for new features.
 ;;
 ;; 2010/09/27
@@ -401,28 +401,28 @@ If nil, it is calculated by `window-width'."
            (set symbol (/ (frame-height) 2))))
   :group 'one-key)
 
-(defcustom one-key-menu-menus-location "~/.emacs.d"
+(defcustom one-key-menus-location "~/.emacs.d"
   "Location in which one-key menus will be stored."
   :type 'directory
   :group 'one-key)
 
-(defcustom one-key-menu-menus-regexp "one-key-menu.*\\.el$"
+(defcustom one-key-menus-regexp "one-key-menu.*\\.el$"
   "Regexp to match filenames of one-key menus.
 If you byte-compile your one-key menus, remember to change the ending of the regexp to elc instead of el."
   :type 'regexp
   :group 'one-key)
 
-(defcustom one-key-menu-auto-load-menus nil
-  "If t then automatically load any files in `one-key-menu-menus-location' with names matching `one-key-menu-menus-regexp'."
+(defcustom one-key-auto-load-menus nil
+  "If t then automatically load any files in `one-key-menus-location' with names matching `one-key-menus-regexp'."
   :type 'boolean
   :group 'one-key)
 
-(defcustom one-key-menu-mode-alist '((one-key-template-mode . one-key-menu-one-key-template))
+(defcustom one-key-mode-alist '((one-key-template-mode . one-key-menu-one-key-template))
   "List of modes and associated one-key menus."
   :type '(alist :key-type symbol :value-type symbol)
   :group 'one-key)
 
-(defcustom one-key-menu-toplevel-alist '((("k" . "one-key") . one-key-menu-one-key))
+(defcustom one-key-toplevel-alist '((("k" . "one-key") . one-key-menu-one-key))
   "The `one-key' top-level alist.
 Contains list of key items for toplevel one-key menu.
 Each item contains a key, description and command, in that order.
@@ -581,7 +581,7 @@ If no region is active then just the current line will be moved."
 	(("C-M-q" . "Indent sexp (C-M-q)") . indent-sexp)
         (("M-TAB" . "Completion At Point (M-TAB)") . completion-at-point)
 	(("C-t" . "Test menu (C-c C-t)") . one-key-template-test-menu)
-	(("C-s" . "Sort commands (C-c C-s)") . one-key-template-sort-map)
+	(("C-s" . "Sort commands (C-c C-s)") . one-key-menu-one-key-template-sort)
 	))
 
 (defun one-key-menu-one-key-template ()
@@ -607,12 +607,12 @@ If no region is active then just the current line will be moved."
 (defun one-key-menu-toplevel ()
   "The `one-key' toplevel menu."
   (interactive)
-  (one-key-menu "toplevel" one-key-menu-toplevel-alist))
+  (one-key-menu "toplevel" one-key-toplevel-alist))
 
 (defun one-key-get-menu (mode)
   "Show appropriate one-key menu for current major mode."
   (interactive `(,major-mode))
-  (let ((menu (assoc mode one-key-menu-mode-alist)))
+  (let ((menu (assoc mode one-key-mode-alist)))
     (if menu
 	(funcall (cdr menu))
       (one-key-menu-toplevel))))
@@ -640,10 +640,10 @@ TITLE is title name of the menu. It can be any string you like."
       (insert (one-key-make-template keymap title))
       (insert "\n;; use the `one-key-get-menu' command to show menu/keybindings for this buffer:\n")
       (insert "\n;; Uncomment and edit following line to set this menu as default for mode.")
-      (insert (concat "\n;;(add-to-list 'one-key-menu-mode-alist '("
+      (insert (concat "\n;;(add-to-list 'one-key-mode-alist '("
                       (replace-regexp-in-string "-map" "" keystroke) " . one-key-menu-" title "))"))
       (insert "\n;; Uncomment and edit following line to add this menu to toplevel menu.")
-      (insert (concat "\n;;(add-to-list 'one-key-menu-toplevel-alist '((\"type key here\" . \""
+      (insert (concat "\n;;(add-to-list 'one-key-toplevel-alist '((\"type key here\" . \""
                       title "\") . one-key-menu-" title "))"))
       ;; Pop to buffer.
       (switch-to-buffer (current-buffer))
@@ -663,10 +663,10 @@ TITLE is title name of the menu. It can be any string you like."
     (insert (one-key-make-template keymap title))
     (insert "\n;; use the `one-key-get-menu' command to show menu/keybindings for this buffer:\n")
     (insert "\n;; Uncomment and edit following line to set this menu as default for mode.")
-    (insert (concat "\n;;(add-to-list 'one-key-menu-mode-alist '("
+    (insert (concat "\n;;(add-to-list 'one-key-mode-alist '("
                     (replace-regexp-in-string "-map" "" keystroke) " . one-key-menu-" title "))"))
     (insert "\n;; Uncomment and edit following line to add this menu to toplevel menu.")
-    (insert (concat "\n;;(add-to-list 'one-key-menu-toplevel-alist '((\"type key here\" . \""
+    (insert (concat "\n;;(add-to-list 'one-key-toplevel-alist '((\"type key here\" . \""
                     title "\") . one-key-menu-" title "))"))
     (forward-line -3)
     (beginning-of-line)))
@@ -1064,8 +1064,8 @@ TITLE is title name that any string you like."
       (buffer-string)
       )))
 
-;; This function is used to load the one-key menus at startup if `one-key-menu-auto-load-menus' is set to t.
-(defun one-key-menu-load-files (filepattern)
+;; This function is used to load the one-key menus at startup if `one-key-auto-load-menus' is set to t.
+(defun one-key-load-files (filepattern)
   "Load all files with paths matching FILEPATTERN"
   (let ((directory (file-name-directory filepattern))
 	(fp (file-name-nondirectory filepattern))
@@ -1087,8 +1087,8 @@ TITLE is title name that any string you like."
       (setq files (cdr files)))))
 
 ;; Load all one-key menus if necessary.
-(if one-key-menu-auto-load-menus
-    (one-key-menu-load-files (concat one-key-menu-menus-location "/.*" one-key-menu-menus-regexp)))
+(if one-key-auto-load-menus
+    (one-key-load-files (concat one-key-menus-location "/.*" one-key-menus-regexp)))
   
 
 (provide 'one-key)
