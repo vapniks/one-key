e0c777847ef976052c6cb174439ce83be2e113ee
Joe Bloggs <vapniks@yahoo.com>
2012-04-14 19:04:32 +0100

    Fix call to one-key-add-elements-to-alist function (forgot to quote one-key-special-keybindings).

--- one-key-dir.el
+++ one-key-dir.el
@@ -200,11 +200,12 @@ but can't go above this dir."
                           (> val 0))))
 
 (customize-set-variable 'one-key-special-keybindings
-                        (one-key-add-elements-to-alist one-key-special-keybindings
-                                '((sort-dir-next "<f3>" "Sort items by next method"
-                                                 (lambda nil (one-key-dir-sort-by-next-method) t))
-                                  (sort-dir-prev "<C-f3>" "Sort items by previous method"
-                                                 (lambda nil (one-key-dir-sort-by-next-method t) t))) t))
+                        (one-key-add-elements-to-alist
+                         'one-key-special-keybindings
+                         '((sort-dir-next "<f3>" "Sort items by next method"
+                                          (lambda nil (one-key-dir-sort-by-next-method) t))
+                           (sort-dir-prev "<C-f3>" "Sort items by previous method"
+                                          (lambda nil (one-key-dir-sort-by-next-method t) t))) t))
 
 (defcustom one-key-dir-special-keybindings
   '(quit-close quit-open toggle-persistence toggle-display next-menu prev-menu up down scroll-down scroll-up
